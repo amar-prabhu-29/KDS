d0ebdc910530434447769a4c176669161510a33f
Amar Prabhu <amarprabhug@gmail.com>
2019-06-20 22:38:08 +0530

    Changes in cards and added sound file and added updates in dashboard

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,14 +1,25 @@
 <template>
     <div class="row">
-        <div class="col l3" v-for="order in orders" v-if="order.orders.length != 0">
+        <div :class="cardsNo" v-for="order in orders" v-if="order.kdsStatus == activeTab && (order.orders.length != 0 || order.completed.length != 0)" v-bind:key="order.OrderKey" style="height: 44vh">
             <div class="card blue-grey darken-1">
                 <div class="card-content white-text" style="padding:0">
-                    <span class="card-title" style="font-size:18px">&nbsp;Table:{{order.table}} In {{order.diner}} Hall<br>&nbsp;Steward:{{order.steward}}</span>
-                        <ul class="collection black-text" style="max-height:33vh;overflow:auto;">
-                            <li class="collection-item" v-for="item in order.orders" v-on:click="itemComplete(item.key)">
+                    <span class="card-title" style="font-size:18px">
+                        &nbsp;Table:{{order.table}} In {{order.diner}} Hall<br>&nbsp;Steward:{{order.steward}}
+                        <a v-if="order.orders.length == 0 && order.completed.length != 0 && activeTab != 'Completed'" class="btn-floating btn-small halfway-fab  waves-effect waves-light green" v-on:click="changeKDS(order.diner,order.table,'Completed')">
+                            <i class="material-icons">check</i>
+                        </a>
+                    </span>
+                        <ul class="collection black-text" style="max-height:30vh;overflow:auto;">
+                            <li class="white" v-if="order.special" style="border-bottom:1px solid black"><marquee><p>{{order.special}}</p></marquee></li>
+                            <li class="collection-item" v-for="item in order.orders" v-on:click="itemComplete(order.diner, order.table, item.order_no)" :style="item.bgColor">
                                 {{item.qty}} X {{item.name}}<span style="float:right">{{item.time | convertTime}}</span>
                                 <br>
-                                <small style="white-space: pre;">{{item.customization | replaceComma}}</small>
+                                <small style="white-space: pre;">{{item.customization | replacePipe}}</small>
+                            </li>
+                            <li class="collection-item" v-for="item in order.completed" :style="item.bgColor">
+                                {{item.qty}} X {{item.name}}<span style="float:right">{{item.time | convertTime}}</span>
+                                <br>
+                                <small style="white-space: pre;">{{item.customization | replacePipe}}</small>
                             </li>
                         </ul>
                 </div>
@@ -18,22 +29,31 @@
 </template>
 
 <script>
+import firebase from 'firebase'
+import firebaseApp from './firebaseInit'
+
 export default {
     name: 'Cards',
-    props: ['orders'],
+    props: ['orders','cardsNo','location','activeTab'],
     data: function(){
         return{
-
+            
         }
     },
     methods:{
-        itemComplete: function(key){
-            console.log(key)
-        }
+        itemComplete: function(diner, table, order_no){
+            let tables = table.split(',')
+            tables.forEach(table => {
+                 firebaseApp.database().ref('Location/'+this.location+'/'+diner+'/Tables/'+table+'/cart/items/'+order_no).update({'status' : "Served"})
+            })      
+        },
+        changeKDS: function(diner,table,status){
+            firebaseApp.database().ref('Location/'+this.location+'/'+diner+'/Tables/'+table).update({'KdsStatus' : status})   
+        },
     },
     filters: {
-        replaceComma: function (value) {
-            return value.split(',').join('\n')
+        replacePipe: function (value) {
+            return value.split('|').join('\n')
         },
         convertTime: function(timestamp) {
             let date = new Date(timestamp*1000)
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,15 +1,34 @@
 <template>
     <div>
-        <nav>
-            <div class="nav-wrapper teal">
-                <router-link to="/" class="brand-logo">KDS</router-link>
-                <ul class="right">
-                    <li><span class="white-text" style="padding-right:5px">Welcome, {{currentUser}}</span></li>
-                    <li><button class="btn black" v-on:click="logout">Logout</button></li>
-                </ul>
-            </div>
-        </nav>
-        <Cards v-if="!loading" v-bind:orders="orders"></Cards>
+        <div class="navbar-fixed">
+            <nav>
+                <div class="nav-wrapper teal">
+                    <a href="#!" class="brand-logo"></a>
+                    <div class="brand-logo center">
+                    <ul>
+                        <li v-if="activeTab!='InProgress'"><a v-on:click="changeTab('InProgress')">Active</a></li>
+                        <li class="active" v-if="activeTab=='InProgress'"><a v-on:click="changeTab('InProgress')">Active</a></li>
+                        <li v-if="activeTab!='Completed'"><a v-on:click="changeTab('Completed')">Completed</a></li>
+                        <li class="active" v-if="activeTab=='Completed'"><a v-on:click="changeTab('Completed')">Completed</a></li>
+                    </ul>
+                    </div>
+                    <ul class="right">
+                    <li>
+                            <select class="black-text" style="display:block;background-color: white;" v-model="kitchenSelect" v-on:change="filterKitchen()">
+                                <option value="All" selected>All</option>
+                                <option v-for="k in kitchens" :value="k" v-bind:key="k">{{k}}</option>
+                            </select>
+                    </li>
+                    <li>
+                        <select class="black-text" style="display:block;background-color: white;" v-model="cardsNo">
+                            <option value="col l4">6</option><option value="col l3">8</option>
+                        </select>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <Cards v-if="!loading" v-bind:orders="orders" v-bind:cardsNo="cardsNo" v-bind:location="location" v-bind:activeTab="activeTab"></Cards>
     </div>
 
 </template>
@@ -28,22 +47,45 @@ export default {
                 location: '',
                 kitchens : [],
                 loading : true,
-                currentUser : false
+                currentUser : false,
+                cardsNo : 'col l3',
+                kitchenSelect : 'All',
+                activeTab : 'InProgress',
+                lengthOrders : 0
             }
         },
     methods:{
+        changeTab : function(tab){
+            this.activeTab = tab
+        },
         getKitchenData : function(){
             let firestore = firebaseApp.firestore()
             firestore.collection('location').doc(this.location).get()
                 .then(
                     documentSnapshot => {
                          documentSnapshot.data().kitchen.forEach(k => {
-                             console.log(k)
                              this.kitchens.push(k)
-                             console.log(this.kitchens)
                         })
                     }
                 )
+        },
+        filterKitchen : function(){
+            if(this.kitchenSelect == 'All'){
+                this.getAllOrdersData()
+            }
+            else{
+                this.getAllOrdersData()
+                this.orders.forEach(order => {
+                    order.orders.forEach(item => {
+                        if(!(this.kitchenSelect === item.kitchen)){
+                            let index = order.orders.indexOf(item)
+                            console.log(index)
+                            let a = order.orders.pop(index)
+                            console.log(a)
+                        }
+                    })
+                })
+            }
         },
         getAllOrdersData : function(){
             let database = firebaseApp.database().ref('Location/'+this.location+'/')
@@ -55,17 +97,34 @@ export default {
                         tables.forEach(table => {
                             if(table.val().Seated){
                                 if(table.val().Status.toLowerCase() === "inprogress"){
-                                    var orderedItems = []
-                                    table.val().cart.items.forEach(item => {
-                                        if(item.status.toLowerCase() === "ordered"){
+                                    let orderedItems = []
+                                    let completedItems = []
+                                    table.child('cart').child('items').forEach(item => {
+                                        if(item.val().status.toLowerCase() === "ordered"){
                                             var itemDetails = {
-                                                'customization': item.customizationlist,
-                                                'name':item.name,
-                                                'qty':item.quantity,
-                                                'key':item.item_id,
-                                                'time':item.ordered_time
+                                                'customization': item.val().customizationlist,
+                                                'name': item.val().name,
+                                                'qty': item.val().quantity,
+                                                'key': item.val().item_id,
+                                                'time': item.val().ordered_time,
+                                                'kitchen': item.val().kitchen,
+                                                'order_no': item.key,
+                                                'bgColor' : 'background-color: white'
                                             }
-                                            orderedItems.push(itemDetails)
+                                            orderedItems.unshift(itemDetails)
+                                        }
+                                        else if(item.val().status.toLowerCase() === "served"){
+                                            var itemDetails = {
+                                                'customization': item.val().customizationlist,
+                                                'name': item.val().name,
+                                                'qty': item.val().quantity,
+                                                'key': item.val().item_id,
+                                                'time': item.val().ordered_time,
+                                                'kitchen': item.val().kitchen,
+                                                'order_no': item.key,
+                                                'bgColor' : 'background-color: green'
+                                            }
+                                            completedItems.push(itemDetails)
                                         }
                                     })
                                     if(!orderIDS.includes(table.val().OrderID)){
@@ -79,19 +138,27 @@ export default {
                                             tableID = "," + table.key
                                         }
                                         tableID = tableID.substring(1);
-                                        this.orders.push({'diner':diners.key,
+                                        this.orders.push({'diner':diners.key, 
                                                         'table':tableID,
                                                         'guests':table.val().NoOfGuests,
                                                         'steward':table.val().StewardName,
                                                         'OrderID':table.val().OrderID,
-                                                        'orders':orderedItems})
+                                                        'OrderKey' : table.val().OrderID,
+                                                        'orders':orderedItems,
+                                                        'completed' : completedItems,
+                                                        'special' : table.val().cart.special_instruction,
+                                                        'kdsStatus' : table.val().KdsStatus})
                                         orderIDS.push(table.val().OrderID)
                                     }
                                 }
                             }
                         })
                     })
-                })   
+                })
+                if(this.lengthOrders < this.orders.length){
+                    console.log("Play Sound")
+                    this.lengthOrders = this.orders.length
+                }             
             },
             err => {
                 console.log(err.message)
